ff38787add00be7e7ad8d9df58ae9c4e50b08c7b
Alessandro Martignano <48163376+T0-B1@users.noreply.github.com>
2024-02-19 15:58:37 +0100

    who needs an IDE

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     console.log("------- REQ -------")
-    console.log(req)
+    console.log(req.headers)
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello, world!\n');
 });
